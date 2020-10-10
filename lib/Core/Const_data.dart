fedb0a68b67c6b36195c278777639755da5911fe
Mevlüt Gür <41743936+mwlt68@users.noreply.github.com>
2020-10-10 15:50:33 +0300

    Update Const_data.dart

--- lib/Core/Const_data.dart
+++ lib/Core/Const_data.dart
@@ -1,5 +1,5 @@
 class ConstData{
   static const String HomeAppBarTitle="Aksaray Gezilecek Yerler";
   static const String ShowDetail="İncele";
-  static const String PlaceDataFirebase="https://vocal-circuit-261521.firebaseio.com/aksaray.json";
+  static const String PlaceDataFirebase="https:firebaseio.com/aksaray.json";
 }
